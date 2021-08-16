ddf98228b83403d63a526e274374fa654c1fec37
autoex <autoex@gmail.com>
2021-08-16 15:28:22 +0300

    search query realization

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import React, {useState} from 'react';
+import React, {useState, useMemo} from 'react';
 import './styles/App.css'
 import Posts from "./components/Posts";
 import Form from "./components/Form";
 import Select from "./UI/select/Select";
+import Input from "./UI/input/Input";
 
 
 const App = () => {
     const [posts, setPosts] = useState([
         {id: 1, title: 'Title', description: 'Description'},
-        {id: 1, title: '111', description: 'dfdf'},
-        {id: 1, title: 'asds', description: 'aaaa'},
+        {id: 2, title: '111', description: 'dfdf'},
+        {id: 3, title: 'asds', description: 'aaaa'},
 
     ]);
     const [selectedSort, setSelectedSort] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
+
+
+    const getSortedPosts =()=> {
+        if(selectedSort) {
+            return [...posts].sort((a, b)=> a[selectedSort].localeCompare(b[selectedSort]))
+        }
+        return posts
+    };
+
+    const sortedPosts = useMemo(()=> {
+        console.log('RUN')
+        if(selectedSort) {
+            return [...posts].sort((a, b)=> a[selectedSort].localeCompare(b[selectedSort]))
+        }
+        return posts
+
+    }, [selectedSort, posts]);
+
+
+    const sortedAndSearchedPosts = useMemo(()=> {
+        return sortedPosts.filter((post)=>post.title.toLowerCase().includes(searchQuery))
+
+    }, [searchQuery, sortedPosts])
 
     const createPost =(post)=> {
         setPosts([...posts, post])
@@ -25,16 +50,24 @@ const App = () => {
     };
     const sortPosts =(sort)=> {
         setSelectedSort(sort);
-        setPosts([...posts.sort((a, b)=> a[sort].localeCompare(b[sort]))])
-        console.log(sort);
-    }
+    };
     return (
         <div className='app'>
             <Form createPost={createPost} />
             <hr style={{marginBottom: '15px'}}/>
-            <Select value={selectedSort} sortPosts={sortPosts} defaultValue='Select' options={[{value: 'title', name: 'By title'}, {value: 'description', name: 'By description'}]}/>
+            <Input
+                placeholder='Search..'
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <Select
+                value={selectedSort}
+                sortPosts={sortPosts}
+                defaultValue='Select'
+                options={[{value: 'title', name: 'By title'}, {value: 'description', name: 'By description'}]}
+            />
 
-            {posts.length === 0 ? 'No posts' : <Posts posts={posts} removePost={removePost}/>}
+            {sortedAndSearchedPosts.length === 0 ? 'No posts' : <Posts posts={sortedAndSearchedPosts} removePost={removePost}/>}
 
 
 
--- src/UI/input/Input.js
+++ src/UI/input/Input.js
@@ -4,7 +4,7 @@ import classes from './Input.module.css'
 const Input = ({...props}) => {
 
     return (
-        <input {...props}/>
+        <input {...props} className={classes.inputTxt}/>
     );
 };
 
