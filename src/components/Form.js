2e6a263579a7476a56fe8663ea4113c0f233c19d
autoex <autoex@gmail.com>
2021-08-16 15:36:49 +0300

    code format

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,7 +8,7 @@ const Form = ({createPost}) => {
     const addPost = (e) => {
         e.preventDefault();
         if (post.title.trim().length === 0 || post.description.trim().length === 0) return;
-        const newPost ={
+        const newPost = {
             id: Date.now(),
             title: post.title,
             description: post.description
@@ -16,19 +16,21 @@ const Form = ({createPost}) => {
         }
 
         createPost(newPost)
-        setPost({...post, title: '', description:''})
+        setPost({...post, title: '', description: ''})
 
     };
     return (
         <form className='form' onSubmit={addPost}>
             <div>
-                <Input  type="text" placeholder={'Title'} value={post.title} onChange={e=>setPost({...post, title:e.target.value})}/>
+                <Input type="text" placeholder={'Title'} value={post.title}
+                       onChange={e => setPost({...post, title: e.target.value})}/>
             </div>
             <div>
-                <Input type="text" placeholder={'Body'} value={post.description} onChange={e=>setPost({...post, description:e.target.value})}/>
+                <Input type="text" placeholder={'Body'} value={post.description}
+                       onChange={e => setPost({...post, description: e.target.value})}/>
             </div>
             <div>
-                <Button >Add</Button>
+                <Button>Add</Button>
             </div>
         </form>
     );
