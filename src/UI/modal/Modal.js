5365ec148f14999d668c577858e87ad838239cf1
autoex <autoex@gmail.com>
2021-08-17 15:02:30 +0300

    modal window change functionality of class changer show/hide popup

--- src/UI/modal/Modal.js
+++ src/UI/modal/Modal.js
@@ -2,8 +2,13 @@ import React from 'react';
 import classes from './Modal.module.css'
 
 const Modal = ({modalActive, setModalActive, children}) => {
+    const rootClasses = [classes.modal__background]
+        if (modalActive) {
+            rootClasses.push(classes.active)
+
+    }
     return (
-        <div className={modalActive ? `${classes.modal__background} ${classes.active}` : `${classes.modal__background}`} onClick={()=>setModalActive(false)}>
+        <div className={rootClasses.join(' ')} onClick={()=>setModalActive(false)}>
                 <div className={classes.modal__window} onClick={e=> e.stopPropagation()}>
                     {children}
                 </div>
