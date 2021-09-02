12be6127511b89ec683f7643a829e887c5268d9e
autoex <autoex@gmail.com>
2021-09-02 13:25:50 +0300

    realization Log out

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -6,15 +6,13 @@ const NavMenu = () => {
     const {isAuth, setIsAuth} = useContext(AuthContext)
     return (
         <div className='navBar'>
-           {isAuth && <ul className="nav nav-pills mb-3">
+            {isAuth && <ul className="nav nav-pills mb-3">
                 <li className="nav-item">
                     <NavLink className="nav-link" to={'/posts'}>Posts</NavLink>
                 </li>
                 <li className="nav-item">
                     <NavLink className="nav-link" to={'/about'}>About</NavLink>
                 </li>
-
-
             </ul>}
             {isAuth &&
             <NavLink onClick={() => setIsAuth(false)} className="nav-link" to={'/about'}>Logout</NavLink>
