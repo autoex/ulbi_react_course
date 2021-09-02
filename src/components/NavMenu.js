518dcc06fe9d7ea4b90e1d022b7b313763162e98
autoex <autoex@gmail.com>
2021-09-02 13:25:50 +0300

    using useContext

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {NavLink} from "react-router-dom";
+import {AuthContext} from "../context";
 
 const NavMenu = () => {
+    const {isAuth, setIsAuth} = useContext(AuthContext)
     return (
-        <ul className="nav nav-pills mb-3">
-            <li className="nav-item">
-                <NavLink className="nav-link" to={'/posts'}>Posts</NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink className="nav-link" to={'/about'}>About</NavLink>
-            </li>
-        </ul>
+        <div className='navBar'>
+           {isAuth && <ul className="nav nav-pills mb-3">
+                <li className="nav-item">
+                    <NavLink className="nav-link" to={'/posts'}>Posts</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to={'/about'}>About</NavLink>
+                </li>
+
+
+            </ul>}
+            {isAuth &&
+            <NavLink onClick={() => setIsAuth(false)} className="nav-link" to={'/about'}>Logout</NavLink>
+            }</div>
     );
 };
 
